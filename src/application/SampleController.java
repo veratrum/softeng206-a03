8b3c43467a763d4cd2002370920d40810ec711e3
Sam <snea076@aucklanduni.ac.nz>
2018-09-16 21:32:23 -0400

    Added the event handler functions for the extra buttons

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -24,18 +24,18 @@ public class SampleController implements Initializable {
 	private ObservableList<Creation> creationDataList;
 	private ObservableList<Creation> selectedCreations;
 	private ObservableList<Recording> recordingDataList;
-	
+
 	private String selectedCreationName;
-	
-	public void handleCreate(){
-		
+
+	public void handleCreateRecording(){
+
 	}
-	
-	public void handleDelete(){
-		
+
+	public void handleDeleteRecording(){
+
 	}
-	
-	public void handlePlay(){
+
+	public void handlePlayRecording(){
 		if (selectedCreations.size() == 0) {
 			return;
 		} else if (selectedCreations.size() == 1) {
@@ -44,21 +44,43 @@ public class SampleController implements Initializable {
 			// ask the user if they want to randomise the order
 			// then play all selected creations
 		}
+		
+		//Finish this one
+	}
+
+	public void handleRate(){
+
+	}
+
+	public void handlePlayCreations(){
+
+	}
+
+	public void handleDeleteCreations(){
+
+	}
+
+	public void handlePlaySelectedCreations(){
+
+	}
+
+	public void handleNewRecording(){
+
 	}
 	
-	public void handleRate(){
-		
+	public void handleNewCreation(){
+
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		creations = new Creations();
-		
+
 		creationDataList = FXCollections.observableArrayList(creations.getCreations());
 		creationList.setItems(creationDataList);
-		
+
 		creationList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-		
+
 		// the first listener detects the last selected creation and shows its recordings in the second list
 		creationList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Creation>() {
 			@Override
@@ -67,7 +89,7 @@ public class SampleController implements Initializable {
 				updateRecordingList();
 			}
 		});
-		
+
 		/* the second listener records any change in the overall selection of multiple recordings, and is used
 		so we know all selected creations when playing multiple in sequence */
 		creationList.getSelectionModel().getSelectedItems().addListener(new ListChangeListener<Creation>() {
@@ -77,14 +99,14 @@ public class SampleController implements Initializable {
 				selectedCreations = (ObservableList<Creation>) change.getList();
 			}
 		});
-		
+
 		if (creations.getCreations().size() > 0) {
 			selectedCreationName = creations.getCreations().get(0).getName();
 		}
-		
+
 		updateRecordingList();
 	}
-	
+
 	/**
 	 * Updates the list of recordings to only show those from the currently selected creation.
 	 */
@@ -93,9 +115,9 @@ public class SampleController implements Initializable {
 		if (creations.getCreations().size() == 0) {
 			return;
 		}
-		
+
 		Creation selectedCreation = creations.getCreationByName(selectedCreationName);
-		
+
 		recordingDataList = FXCollections.observableArrayList(selectedCreation.getRecordings());
 		recordingList.setItems(recordingDataList);
 	}
