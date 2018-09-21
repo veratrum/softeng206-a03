56a6499a141bc51690fe2b67fc0182c2e7974c26
Sam <snea076@aucklanduni.ac.nz>
2018-09-20 22:24:14 -0400

    Fixed the naming of the files, package structure, etc

--- src/namesayer/Creations.java
+++ src/namesayer/Creations.java
@@ -1,4 +1,4 @@
-package application;
+package namesayer;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- src/namesayer/Main.java
+++ src/namesayer/Main.java
@@ -1,4 +1,4 @@
-package application;
+package namesayer;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("NamesayerGUI.fxml"));
 			Scene scene = new Scene(root,800,650);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Namesayer 3.0");
